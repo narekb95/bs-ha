d7e26233a7058f78e6c0658b220e1bd0bc59954a
Narek Bojikiain <narekbojikiain@Nareks-MacBook-Air.local>
2018-05-25 22:54:21 +0200

    Feature: server side

--- remoteshell/echo_server/server.c
+++ remoteshell/echo_server/server.c
@@ -32,23 +32,25 @@ int main()
 	if (bind(sfd, (struct sockaddr*) &srv_addr, sad_sz) < 0)
 		die("Couldn't bind socket");
 	
-	if (listen(sfd, 1) < 0)
-		die("Couldn't listen to the socket");
-	
-	cfd = accept(sfd, (struct sockaddr*) &cli_addr, &sad_sz);
-	if (cfd < 0)
-		die("Couldn't accept incoming connection");
-	
-	while ((bytes = read(cfd, buf, sizeof(buf))) != 0)
-	{
-		if (bytes < 0)
-			die("Couldn't receive message");
-		
-		if (write(cfd, buf, bytes) < 0)
-			die("Couldn't send message");
-	}
-	
-	close(cfd);
-	close(sfd);
+    if (listen(sfd, 1) < 0)
+        die("Couldn't listen to the socket");
+    while(1)
+    {
+        cfd = accept(sfd, (struct sockaddr*) &cli_addr, &sad_sz);
+        if (cfd < 0)
+            die("Couldn't accept incoming connection");
+        
+        while ((bytes = read(cfd, buf, sizeof(buf))) != 0)
+        {
+            if (bytes < 0)
+                die("Couldn't receive message");
+            
+            if (write(cfd, buf, bytes) < 0)
+                die("Couldn't send message");
+        }
+        
+    }
+    close(cfd);
+    close(sfd);
 	return 0;
 }
