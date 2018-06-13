849a36faf67a2d31ccd2ec52c0ce94b71278388c
Unknown <piotrekwitkowski1@gmail.com>
2018-06-13 09:48:48 +0200

    HA5 Vorlage uploaded

---
+++ 5_taxCollecotrs/impl_Piotrek/cmake-build-debug/CMakeFiles/impl_Piotrek.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/impl_Piotrek.dir/tcd.c.o"
+  "impl_Piotrek.pdb"
+  "impl_Piotrek"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/impl_Piotrek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
